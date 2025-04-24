1d5816a7fc6b4406a0c0107c56792239e6c1674f
David Felipe Cabarique Durán <98673133+Cabariqued27@users.noreply.github.com>
2025-04-24 11:57:26 -0500

    update

--- tyba_university/lib/modules/home/controllers/detailed_controller.dart
+++ tyba_university/lib/modules/home/controllers/detailed_controller.dart
@@ -36,23 +36,55 @@ class DetailedController extends GetxController {
     String savedImagePath = universityInformation?.imagePath ?? '';
     if (savedImagePath.isNotEmpty) {
       image.value = savedImagePath;
-    } else if (universityInformation?.imagePath?.isNotEmpty == true) {
-      image.value = universityInformation!.imagePath!;
     }
   }
 
   Future<void> validatePhotosPermission() async {
     try {
-      await onChangeProfileImage();
+      await _showImagePickerDialog();
     } catch (e, stack) {
       LogError.capture(e, stack, 'validatePhotosPermission');
       Get.snackbar('Permiso denegado', 'No se pudo acceder a la galería');
     }
   }
 
-  Future<void> onChangeProfileImage() async {
+  
+  Future<void> _showImagePickerDialog() async {
+    return showDialog<void>(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Selecciona una opción'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Tomar foto'),
+                onTap: () {
+                  _pickImage(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.image),
+                title: const Text('Seleccionar de la galería'),
+                onTap: () {
+                  _pickImage(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Función para manejar la toma de foto o selección de imagen
+  Future<void> _pickImage(ImageSource source) async {
     try {
-      final fileData = await _picker.pickImage(source: ImageSource.gallery);
+      final fileData = await _picker.pickImage(source: source);
 
       if (fileData != null) {
         final file = File(fileData.path);
